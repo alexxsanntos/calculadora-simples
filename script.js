acc9433fe9dab985ecb81467ad3de3d0df338784
Alex Santos <78109568+alexxsanntos@users.noreply.github.com>
2025-04-02 18:29:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
-//Define function to calculate based on button clicked.
+//Define função para calcular baseado no botão clicado.
 const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
-    //If output has '%', replace with '/100' before evaluating.
+    //Se a saída for '%', subsititui para '/100'.
     output = eval(output.replace("%", "/100"));
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
-    //If DEL button is clicked, remove the last character from the output.
+    //Se DEL é clicado, remove o último número digitado.
     output = output.toString().slice(0, -1);
   } else {
-    //If output is empty and button is specialChars then return
+    //Se a saída está vazia e o botão é caractere especial
     if (output === "" && specialChars.includes(btnValue)) return;
     output += btnValue;
   }
   display.value = output;
 };
-//Add event listener to buttons, call calculate() on click.
+//Adiciona evento aos botões para calcular
 buttons.forEach((button) => {
-  //Button click listener calls calculate() with dataset value as argument.
+  //Botão chama o calculate() com o valor do dataset.
   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
