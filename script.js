8a97e819e247e4ef9eae36c24fafbc2c2b9b4aad
Alex Santos <78109568+alexxsanntos@users.noreply.github.com>
2025-04-02 18:59:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,26 +2,32 @@ const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
-//Define função para calcular baseado no botão clicado.
+//Defina a função para calcular com base no botão clicado.
+
 const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
-    //Se a saída for '%', subsititui para '/100'.
+    //Se a saída tiver '%', substitua por '/100' antes de avaliar.
+
     output = eval(output.replace("%", "/100"));
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
-    //Se DEL é clicado, remove o último número digitado.
+    //Se o botão Del for clicado, remova o último caractere da saída.
+
     output = output.toString().slice(0, -1);
   } else {
-    //Se a saída está vazia e o botão é caractere especial
+    //Se a saída estiver vazia e o botão for specialchars, retorne
+
     if (output === "" && specialChars.includes(btnValue)) return;
     output += btnValue;
   }
   display.value = output;
 };
-//Adiciona evento aos botões para calcular
+//Adicione o ouvinte de eventos aos botões, ligue para calcular () no clique.
+
 buttons.forEach((button) => {
-  //Botão chama o calculate() com o valor do dataset.
+  //Botão Clique em chamadas de ouvinte Calcule () com o valor do conjunto de dados como argumento.
+
   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
